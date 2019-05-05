205685574fb555199f929d57d60fdaad713e24a6
Alden Greene <46254363+aldeng323@users.noreply.github.com>
2019-05-05 10:00:41 -0700

    modified all aspects of pages

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
-$('.menu').on('click', function () {
-    $(this).toggleClass('active');
+const navSlide = () => {
+    /* const burger = document.querySelector('.menu');
+    const nav = document.querySelector('.nav');
+    const navLinks = document.querySelectorAll('.bar');
 
-})
+// Toggle Nav
+    burger.addEventListener('click', () => {
+
+        nav.classList.toggle('nav');
+
+        
+       navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            }else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0}s`;
+        }
+    });
+
+    // burger animation
+    burger.classList.toggle('navLinks');
+    });
+
+}
+
+navSlide();
+
+*/
